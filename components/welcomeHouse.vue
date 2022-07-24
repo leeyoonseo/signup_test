3a5667a8f00964bac66035da6d6bbf1849fb0404
leeyoonseo <okayoon.lee@gmail.com>
2022-07-24 22:54:29 +0900

    회원가입 완료 페이지

--- components/welcomeHouse.vue
+++ components/welcomeHouse.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <span>{{ `\`홍길동\`님 회원가입 되었습니다.` }}</span><br/>
-    <span>{{ `이메일: abc@google.com` }}</span><br/>
-    <span>{{ `주소: 00시 00구 00동 123번지 1층` }}</span><br/>
-    <span>{{ `연락처: 000-0000-0000` }}</span><br/>
+    <span>{{ `\`${user.name}\`님 회원가입 되었습니다.` }}</span><br/>
+    <span>{{ `이메일: ${user.email}` }}</span><br/>
+    <span>{{ `주소: ${user.address} ${user.detailAddress}` }}</span><br/>    
+    <span>{{ `연락처: ${user.phone}` }}</span>    
   </div>
 </template>
 <script>
+import { mapGetters } from 'vuex';
 export default {
-  name: 'WelcomeHouse'
+  name: 'WelcomeHouse',
+  data() {
+    return {}
+  },
+  computed: {
+    ...mapGetters({
+      user: 'user'
+    }),
+  }
 }
 </script>
